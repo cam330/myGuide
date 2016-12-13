16787f2e1ae2e2adcd54f3bc53dafd5645eca29f
cam330 <wilcoxcam@gmail.com>
2016-12-13 08:57:16 -0800

    updated map selection

--- app/guide/chooseMap.controller.js
+++ app/guide/chooseMap.controller.js
@@ -18,25 +18,19 @@
 
         vm.placesArray = [
         {country: "Spain", 
-        attraction:["Palacio Real", "Aqueduct of Segovia", "Sagrada Familia", "El Escorial", "Mezquita of Cordoba", "Alhambra"]}, 
-        {country: "Mexico",
-        attraction:["Guanajuato", "Palenque", "Tulum", "Chichen Itza", "Teotihuacan"]},
-        {country:"China",
-        attraction: ["Mogao Caves", "Leshan Giant Buddha", "Terracotta Army", "Forbidden City", "Potala Palace", "Great Wall"]},
+        attraction:["Sagrada Familia", "El Escorial", "Alhambra"]}, 
+        {country: "Japan",
+        attraction:["Horyu-ji"]},
         {country :"France",
-        attraction : ["Sainte Chapelle", "Centre Pompidou", "Mesee d'Orsay", "Jardin du Luxembourg", "Notre Dame", "Sacre-Coeur", "Arc de Triomphe", "Louvre", "Eiffel Tower"]},
+        attraction : ["Notre Dame", "Arc de Triomphe", "Eiffel Tower"]},
         {country:"Germany",
-        attraction: ["Frauenkirche", "Lindau", "Cologne Cathedral", "Holstentor", "Heidelberg Old City", "Brandenburg Gate", "Neuschwanstein"]},
+        attraction: ["Reichstag Building","Neuschwanstein"]},
         {country:"Italy",
-        attraction: ["San Gimignano", "Cinque Terre", "Leaning Tower of Pisa", "Pompeii", "Piazza del Campo", "Santa Maria del Fiore", "Vatican City", "Colosseum", "St Mark's Basilica"]},
-        {country:"Russia",
-        attraction: ["St Sophia Cathedral, Novgorod", "Suzdal", "Moscow Kremlin", "Hermitage Museum", "Saint Basil's Cathedral"]},
-        {country:"Turkey",
-        attraction: ["Aspendos Theatre", "Bodrum Castle", "Mount Nemrut", "Blue Mosque", "Library of Celsus", "Hagia Sophia"]},
+        attraction: ["Colosseum", "Pantheon", "Florence Cathedral"]},
         {country:"United Kingdom",
-        attraction: ["Warwick Castle", "Tower of London", "Durham Cathedral", "York Minster", "Windsor Castle", "Big Ben", "Stonehenge"]},
+        attraction: ["Tower of London", "Buckingham Palace", "Stonehenge"]},
         {country:"United States",
-        attraction: ["White House", "Denali National Park", "Las Vegas Strip", "Kilauea", "Niagara Falls", "Golden Gate Bridge", "Yellowstone", "Manhattan", "Grand Canyon"]}];
+        attraction: ["White House", "Statue of Liberty"]}];
 
         activate();
 
